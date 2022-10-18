8e8080eecad0a43abb7bd0a0c3b88857014ffd7f
Koby Shaiseththah Outama <koutama@circinus-26.ics.uci.edu>
2022-10-18 00:31:30 -0700

    Finished stage 2

--- assignment_2/stage2/hw2.c
+++ assignment_2/stage2/hw2.c
@@ -2,12 +2,31 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <signal.h>
 
+#define MAXJOBS 5
 #define MAXTOKENS 64
 #define MAXLINE 80
 #define BUFFERSIZE 256
 
+pid_t fg; 
+pid_t pid;
+
+void chldint_handler(int sig){
+    if(sig == SIGINT)
+        exit(0);
+    }
+
+void prntint_handler(int sig){
+    }
+
 void eval(char **args){
+    int child_stat;
+
     if (!strcmp("pwd", args[0])){
         char dir[BUFFERSIZE];
         printf("%s\n", getcwd(dir, BUFFERSIZE));
@@ -15,11 +34,46 @@ void eval(char **args){
     else if (!strcmp("cd", args[0])){
         chdir(args[1]);
     }
+    else{
+        int err;
+
+        if ((pid = fork()) == 0){
+
+            fg = getpid();
+            signal(SIGINT, prntint_handler);
+
+            // child process
+            // signal goes here, after fork, before exec
+            err = execv(args[0], args);
+            err = execvp(args[0], args);
+            printf("s");
+            exit(err);
+        }
+        else if(pid == -1){
+            printf("ERROR CHILD NOT CREATED\n");
+            
+        }
+        else{
+            // parent process
+            //kill(pid, SIGINT);
+            //wait(NULL);
+            waitpid(fg, &child_stat, 0);
+            // wait(&child_stat);
+            // there is to implement waiting for program
+            // if(NULL == argv[i]) waitpid(pid, NULL, 0);
+        }      
+    }
 }
 
 void get_cmd(char *line, char **args){
+    /* line: is a string with tabs or spaces to space words
+    *  args: pointer to return parsed string
+    *  Return:
+    *         An array of tokens. 
+    */
+
     char *token;
-    printf("prompt > ");                   
+    printf("prompt >");                   
     fgets(line, MAXLINE, stdin); 
     const char* delims = " \t\n";
     
@@ -35,9 +89,11 @@ void get_cmd(char *line, char **args){
 
 int main(int argc, char** argv){
     char cmdline[MAXLINE]; 
-    char *args[MAXTOKENS]; 
+    char *args[MAXTOKENS];
     // char **ptr = args;
     while (1) {
+        signal(SIGINT, prntint_handler);
+
         get_cmd(cmdline, args);
         // printf("%s\n", cmdline);
         // while(*ptr != 0){
