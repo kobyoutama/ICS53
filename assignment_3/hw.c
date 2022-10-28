068d8b1ecb9e0821b8e4276f29fd381637fc9cdd
Koby Shaiseththah Outama <koutama@circinus-43.ics.uci.edu>
2022-10-27 19:45:32 -0700

    Finished FIFO, TO DO: PageTableBits/LRU

--- assignment_3/hw.c
+++ assignment_3/hw.c
@@ -28,6 +28,12 @@ struct PageBlock p_table[VM_ADDRS/PAGE_OFFSET];
 
 int page_algo, fifo_queue = 0;
 
+void printALL(){
+    for (int i=0; i<VM_ADDRS; i++){
+        printf("disk_memory[%d] = %d\n", i, disk_memory[i]);
+    }
+}
+
 void initialize_pages(){
     for (int i = 0; i < VM_ADDRS/PAGE_OFFSET; i++){
         
@@ -37,7 +43,7 @@ void initialize_pages(){
     }
     for (int i = 0; i < MM_ADDRS; i++){
 
-            main_memory[i].address = i;
+            main_memory[i].address = -1;
             main_memory[i].data = -1;
     }
 }
@@ -77,29 +83,40 @@ void show_memblock(int ppn){
         printf("Out Of Bound Main Memory Page : %d\n", ppn);
         return;
     }
-
     for (int offset=0; offset<PAGE_OFFSET; offset++){
         printf("%d: %d\n", (ppn*PAGE_OFFSET)+offset, main_memory[(ppn*PAGE_OFFSET)+offset].data);
     }
 }
+
+void evict(int vm_addr, int mem_addr){
+    for(int i=0; i<PAGE_OFFSET; i++){
+        disk_memory[vm_addr+i] = main_memory[mem_addr+i].data;
+    }
+}
+
 // 0
-void FIFO(int vm_addr, int data){
+void FIFO(int vm_addr, int data, int rw){
     int start_addr = floor(vm_addr/PAGE_OFFSET)*PAGE_OFFSET;
     if (fifo_queue == MM_ADDRS/PAGE_OFFSET) fifo_queue = 0;
+    // TODO : EVICT ONLY WHEN DIRTY BIT == 1, CHANGE VALID BIT TO 0, DO PAGE NUMBER 
+    if (main_memory[(fifo_queue*PAGE_OFFSET)].address != -1) evict(main_memory[(fifo_queue*PAGE_OFFSET)].address, fifo_queue*PAGE_OFFSET);
     for(int i=0; i<PAGE_OFFSET; i++){
         main_memory[(fifo_queue*PAGE_OFFSET)+i].address = start_addr+i;
+            // printf("PUTTING %d in main_memory[%d]\n", data, fifo_queue+i);
 
-        if (main_memory[(fifo_queue*PAGE_OFFSET)+i].address==vm_addr) {
+        if (!rw && main_memory[(fifo_queue*PAGE_OFFSET)+i].address==vm_addr) {
             // printf("PUTTING %d in main_memory[%d]\n", data, fifo_queue+i);
             main_memory[(fifo_queue*PAGE_OFFSET)+i].data = data;
-            p_table[start_addr/PAGE_OFFSET].valid_bit = 1;
             p_table[start_addr/PAGE_OFFSET].dirty_bit = 1;
-            p_table[start_addr/PAGE_OFFSET].page = (fifo_queue*PAGE_OFFSET);
             }
         else
 
             main_memory[(fifo_queue*PAGE_OFFSET)+i].data = disk_memory[vm_addr+i];
     }
+    
+    p_table[start_addr/PAGE_OFFSET].page = (fifo_queue*PAGE_OFFSET);
+    p_table[start_addr/PAGE_OFFSET].valid_bit = 1;
+
     fifo_queue++;
 }
 
@@ -107,39 +124,46 @@ void FIFO(int vm_addr, int data){
 void LRU(int vm_addr){
 
 }
-void to_mmem(int va, int num){
+void to_mmem(int va, int num, int rw){
     // FIFO
     if(!page_algo){
-        FIFO(va, num);
+        FIFO(va, num, rw);
     }
     else{
         LRU(va);
     }
 }
 
-int check_mmem(int va, int print){
-    for (int i=0; i<=MM_ADDRS; i++){
-        // Will need to fix later, assumes .data will always be -1 when adding new data
-        if(main_memory[i].address == va && main_memory[i].data != -1){
-            if (print) printf("%d\n",main_memory[i].data);
+int check_mmem(int va, int rw){
+    // [read] rw = 1, [write] rw = 0
+    for (int i=0; i<MM_ADDRS; i++){
+        if(main_memory[i].address == va){
+            if (rw) printf("%d\n",main_memory[i].data);
             return 0;
         }
     }
     printf("A Page Fault Has Occurred\n");
-    if (print) printf("%d\n", disk_memory[va]);
+    if (rw){
+        to_mmem(va, -1, rw);
+        printf("%d\n", disk_memory[va]);
+    } 
     return 1;
-}
-
-void print_mem(int va){
-    check_mmem(va, 1);
-
-    printf("%d\n",main_memory[va].data);
 }
 
 void write_memblock(int va, int num){
     // Not in memory
-    if (check_mmem(va, 1)){
-        to_mmem(va, num);
+    if (check_mmem(va, 0)){
+        to_mmem(va, num, 0);
+    }
+    else{
+        for (int i=0; i<MM_ADDRS; i++){
+            if(main_memory[i].address == va){
+                main_memory[i].data = num; 
+                int start_addr = floor(va/PAGE_OFFSET)*PAGE_OFFSET;
+                p_table[start_addr/PAGE_OFFSET].dirty_bit = 1;
+                return;
+            }
+        }
     }
 }
 
@@ -160,6 +184,8 @@ int main(int argc, char **argv){
         else if (!strcmp(args[0], "write")) write_memblock(atoi(args[1]), atoi(args[2]));
         else if (!strcmp(args[0], "showmain")) show_memblock(atoi(args[1]));
         else if (!strcmp(args[0], "showptable")) show_pagetable();
+        else if (!strcmp(args[0], "print")) printALL();
+
     }
     return 0;
 }
