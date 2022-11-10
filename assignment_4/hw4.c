6808a9b0bc8b701f8e158bd427d60ee5f839950e
Koby Shaiseththah Outama <koutama@circinus-36.ics.uci.edu>
2022-11-10 15:46:39 -0800

    finshed assignment4 and conform to c style guide/docu

--- assignment_4/hw4.c
+++ assignment_4/hw4.c
@@ -13,6 +13,7 @@
 
 
 int fit_algo = 0;
+int block_ct = 1;
 char heap[HEAP_SIZE] = { [0 ... (HEAP_SIZE-1)] = 0 };
 
 struct BlockList{
@@ -20,13 +21,6 @@ struct BlockList{
     int start;
     struct BlockList *next; 
 };
-
-struct BlockLIst *list = NULL;
-
-/*  TODO:               */
-/*        BLOCKLIST     */
-/*        Double check does not split for bytes < 3 */
-
 
 int msb(int i){
     /* function returns value of the 7 most significant bits in decimal */
@@ -43,6 +37,7 @@ void to_bits(int n){
 }
 
 void print_heap(){
+    /* DEBUG FUNCTION : prints heap memory*/
     for (int i=0; i<HEAP_SIZE; i++){
         printf("%d : %d\n", i, heap[i]);
     }
@@ -50,7 +45,7 @@ void print_heap(){
 
 void user_input(char *line, char **args){
     /* Parse user inputs */
-    printf("> ");                   
+    printf(">");                   
     fgets(line, MAX_LINE, stdin); 
     
     char *token = NULL;
@@ -64,8 +59,10 @@ void user_input(char *line, char **args){
         token = strtok(NULL, delims);
     }
 }
-void addBlock(int p, int len){
-    int newsize = ((len+2)<<1);
+
+void add_block(int p, int len){
+    /* Function adds a block of length "len" given a pointer "p" */
+    int newsize = ((len+2)<<1);       // calculate bits of new size
     int oldsize = heap[p]&-2;         // mask out low bit
     heap[p] = newsize|1;              // set new length
     
@@ -77,6 +74,7 @@ void addBlock(int p, int len){
         int newfree = ((msb(oldsize)-msb(newsize))<< 1) &-2;
         heap[p+msb(newsize)] = newfree;
         heap[p+msb(oldsize)-1] = newfree;
+        block_ct += 1;
     }
     else{
         // set length in remaining part of block (footer)
@@ -105,6 +103,7 @@ void deallocate(int p){
         // checks if next block is free
         if(!(heap[next_head] & 1)){
             // if free, coalesce blocks
+            block_ct -= 1; 
             int next_sz = msb(heap[next_head])-2;
             heap[next_head] = heap[next_head-1] = 0;
 
@@ -125,6 +124,7 @@ void deallocate(int p){
         // checks if previous block is free
         if(!(heap[prev_footer] & 1)){
             // if free, coalesce blocks
+            block_ct -= 1; 
             int prev_sz = msb(heap[prev_footer])-2;
             heap[prev_footer] = heap[prev_footer+1] = 0;
 
@@ -137,27 +137,23 @@ void deallocate(int p){
 
 
 void allocate(int size){
-    int p = 0;
-    int found = 0; // starts ptr at index 0 
- 
+    /* Function allocates a block of size "size" into heap */
+
+    int p = 0; // starts ptr at index 0
     /* Allocate using First Fit */
     if (!fit_algo){
         // printf("First Fit\n");
-        while ((p < HEAP_SIZE) && 
-        ((heap[p] & 1) || 
-        msb(heap[p]) < size))   
-        {
-            found = 1;
+        while ((p < HEAP_SIZE) && ((heap[p] & 1) || msb(heap[p]) < size))   
+            // sets that something is found and iterate to next block
             p = p + (msb(heap[p] & -2));
-        }
+        
         // only add block if there is an open block
-        if(found){
-            addBlock(p, size);  
+        if(p < HEAP_SIZE-1 && size <= HEAP_SIZE-2){
+            add_block(p, size);  
             printf("%d\n",p+1);
             return;
         }
     }
-
     /* Allocate using Best Fit */
     else{
         //printf("Best Fit\n");
@@ -166,10 +162,8 @@ void allocate(int size){
 
         // cycle through entire heap
         while(p<HEAP_SIZE){
-
             // check if it is unallocated and the size < unallocated block size
             if((!(heap[p]&1)) && size < msb(heap[p])){
-
                 // if the current unallocated block is smaller than the saved block
                 if(smallest_size > msb(heap[p])){
                     // remember pointer location and size
@@ -177,31 +171,28 @@ void allocate(int size){
                     smallest_p = p;
                 }
             }
-
             // iterate to next block
             p = p + (msb(heap[p] & -2));
         }
 
         // if pointer is -1, could not find a block, so skip adding and print error 
-        if(smallest_p != -1){
-            addBlock(smallest_p, size);  
+        if(smallest_p != -1 && size <= HEAP_SIZE-2){
+            add_block(smallest_p, size);  
             printf("%d\n",smallest_p+1);
             return;
         }
-
     }  
-    
     printf("Heap Full : Unsuccessful Malloc of Size :%d\n", size);
 }
 
-void writemem(int p, char* str){
+void write_mem(int p, char* str){
     /* function writes ascii value of characters into heap */
     /*     *** Does not do explicit bounds-checking ***    */
     for (int i=0; i<strlen(str); i++)
         heap[p+i] = (int) str[i]; 
 }
 
-void printmem(int p, int len){
+void print_mem(int p, int len){
     /*     function prints decimal value of heap memory    */
     /*     *** Does not do explicit bounds-checking ***    */
     for(int i=0; i<len; i++)
@@ -210,21 +201,73 @@ void printmem(int p, int len){
 }
 
 void blocklist(){
+    /* Function prints list of blocks ordered by payload size */
+
     int p = 0;
-    printf("BLOCKLIST : not implemented\n");
-    // while (p < HEAP_SIZE){
-    //     if(heap[p] & 1){
-    //         if(!head){
-    //             head = BlockList {1,1,NULL};
-    //         }
-    //     }
-    //     p = p + (msb(heap[p] & -2));
-    // }
-    
+    struct BlockList *head = NULL;
+
+    // Loop through heap
+    while(p<HEAP_SIZE){
+
+        // Create linked list of blocks
+        struct BlockList* new_block = malloc(sizeof(struct BlockList));
+        new_block->size = heap[p];
+        new_block->start = p;
+        new_block->next = NULL;
+
+        // Creates the link list if first block
+        if(head == NULL){
+            head = new_block;
+        }
+        // Adds block as the new head if the new block size is < head size
+        else if(msb(new_block->size) > msb(head->size)){
+            new_block->next = head;
+            head = new_block;
+        }
+        // For cases after first block is added into linked list
+        else {
+            struct BlockList* temp = head;
+            while(temp != NULL){
+                // Adds block to end of the linked list
+                if(msb(new_block->size) <= msb(temp->size) && temp->next == NULL){
+                    temp->next = new_block;
+                    break;
+                }
+                // Adds new block inbetween blocks i.e. temp -> new block -> (temp->next)
+                else if(msb(new_block->size) <= msb(temp->size) &&
+                  msb(new_block->size) > msb(temp->next->size)){
+                    new_block->next = temp->next;
+                    temp->next = new_block;
+                    break;
+                }
+                temp = temp->next;
+            }
+        }
+        // go to new block
+        p = p + (msb(heap[p] & -2));
+    }
+
+    // print out order by decending order on payload size
+    while(head!=NULL){
+        if (!(head->size&1))
+            printf("%d-%d-free\n",msb(head->size)-2,head->start+1);
+        else
+            printf("%d-%d-allocated\n",msb(head->size)-2,head->start+1);
+
+        head = head->next;
+    }
+
+    // free all blocks in linked list
+    while(head != NULL){
+        struct BlockList* temp = NULL;
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
 }
 
-void initializeHeap(){
-    // set header/footer size of heap and sets LSB to 0 
+void initialize_heap(){
+    // set header/footer size of heap and LSB to 0 
     int size = ((HEAP_SIZE) << 1) & -2; 
     heap[0] = size;
     heap[HEAP_SIZE-1] = size; 
@@ -232,7 +275,7 @@ void initializeHeap(){
 
 
 int main(int argc, char **argv){
-    initializeHeap();
+    initialize_heap();
 
     if(argc > 1 && strcmp(argv[1], "BestFit\n")) fit_algo = 1;
     
@@ -245,9 +288,11 @@ int main(int argc, char **argv){
         if (!strcmp(args[0], "quit")) return 0;
         else if (!strcmp(args[0], "malloc")) allocate(atoi(args[1]));
         else if (!strcmp(args[0], "free")) deallocate(atoi(args[1]));
-        else if (!strcmp(args[0], "writemem")) writemem(atoi(args[1]),args[2]);
-        else if (!strcmp(args[0], "printmem")) printmem(atoi(args[1]),atoi(args[2]));
+        else if (!strcmp(args[0], "writemem")) write_mem(atoi(args[1]),args[2]);
+        else if (!strcmp(args[0], "printmem")) print_mem(atoi(args[1]),atoi(args[2]));
         else if (!strcmp(args[0], "blocklist")) blocklist();
+        
+        // Functions below are meant for debugging purposes
         else if (!strcmp(args[0], "print")) print_heap();
         else if (!strcmp(args[0], "printb")) to_bits(heap[atoi(args[1])]);
 
