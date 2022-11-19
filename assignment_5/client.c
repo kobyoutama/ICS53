a4798161f401ed2f776cd0797b1c4f6c2e39c91d
Koby Shaiseththah Outama <koutama@circinus-96.ics.uci.edu>
2022-11-19 06:33:32 -0800

    finished tcp data transfer

--- assignment_5/client.c
+++ assignment_5/client.c
@@ -1,20 +1,78 @@
 // Koby Outama koutama
 /* client.c */
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
+#define MAX_TOKENS 16
+#define BUFFER_SIZE 256
+#define PORT 32222
 
-void user_input(char *line, char **args){
+void user_input(char *line, char *buf){
     /* Parse user inputs */
-    printf(">");                   
-    fgets(line, MAX_LINE, stdin); 
     
-    char *token = NULL;
-    const char* delims = " \t\n";
+    printf("> ");
+    fgets(line, BUFFER_SIZE, stdin); 
+    line[strcspn(line,"\n")] = '\0';
     
-    token = strtok(line, delims);
-
-    while (token != NULL){
-        *args = token;
-        args++;
-        token = strtok(NULL, delims);
+    // limits characters to 255 
+    if(strlen(line)>= BUFFER_SIZE-1){
+        // flush stdin 
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) { }
+        printf("Invalid Syntax\n");
+        return;
     }
+
+    // concatenates length with characters
+    // buf[1] = '\0' ensures string is reset
+    buf[0] = strlen(line);
+    buf[1] = '\0';
+    strcat(buf, line);
+}
+
+int main(int argc, char const* argv[])
+{
+    char buffer[BUFFER_SIZE] = { 0 };
+    char cmd_line[BUFFER_SIZE] = { 0 };
+    int sock = 0, valread, client_fd;
+    struct sockaddr_in serv_addr;
+        
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    
+    if (inet_pton(AF_INET, "127.0.0.1", 
+    &serv_addr.sin_addr)<= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+    
+    if ((client_fd= connect(sock, (struct
+    sockaddr*)&serv_addr, sizeof(serv_addr)))< 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    user_input(cmd_line, buffer);
+
+    while(1){
+        
+            send(sock, buffer, BUFFER_SIZE, 0);
+            printf("[SENT] message: %s\n", buffer);
+            valread = read(sock, buffer, BUFFER_SIZE);
+            if(!strcmp(cmd_line,"quit")){break;}
+            printf("[RECV] message: %s\n", buffer);
+            // printf("%s\n", buffer);
+            // printf("ASDASDAD\n");
+            user_input(cmd_line, buffer);
+    }
+    printf("CLOSING CLIENT\n");
+    close(sock);
+    return 0;
 }
--- assignment_5/server.c
+++ assignment_5/server.c
@@ -4,8 +4,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
 
+#define PORT 32222
 #define MAX_LINE 256
+
+int server_fd;
+struct sockaddr_in address;
+
+
+
+void int_handler(int sig){
+    shutdown(server_fd, SHUT_RDWR);
+    printf("\nServer Closed\n");
+    exit(0);
+}
 
 void user_input(char *line, char **args){
     /* Parse user inputs */
@@ -25,15 +41,84 @@ void user_input(char *line, char **args){
 }
 
 
-int main(int argc, char** argv){
-    /* ICS Avaliable ports 30000 - 60000 */
+void start_server(int port){
+    /* variables */
+
+    int opt = 1;
+
+    /* create socket */
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_fd, SOL_SOCKET,SO_REUSEADDR, &opt,sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    /* call bind */
+    if (bind(server_fd, (struct
+        sockaddr*)&address,sizeof(address))< 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* listen */
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    /* connection established */
+    printf("server started\n");
+}
+
+void connection(){
+    char buf[256]= { 0 };
+    char buffer[256] = { 0 }; 
+    int new_socket, valread;
+    int addrlen = sizeof(address);
+    if ((new_socket= accept(server_fd, (struct
+        sockaddr*)&address,(socklen_t*)&addrlen))< 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    printf("New Connection Established\n");
+    while(1){
+        // change atoi(buffer) to buffer[0] after implementing bytes
+        
+        if(read(new_socket, buffer, 256)> 0){
+        printf("[RECV] sz:%d msg:%s\n", buffer[0], buffer);
+        for(int i=0; i<buffer[0]; i++){
+            buf[i] = buffer[i+1];
+        }
+        buf[buf[0]+1] = '\0';
+        if(!strcmp(buf, "quit"))
+            break;
+        }
+        send(new_socket, "VALID\0", 6, 0);
+        printf("[SENT] VALID\n");
+        
+   
+    }
+    close(new_socket);
+    printf("Connection Closed\n");
+}
+
+int main(int argc, char **argv)
+{
     char* pfe_path = argv[1];
     char* mrna_path = argv[2];
-    int port = atoi(argv[3]);
-
-    /* READ FILES AND START SERVER */
-    printf("%s %s %d\n", argv[1], argv[2], port);
-    printf("server started\n");
-    
+    int addrlen;
+    int port = PORT; // Fix PORT to atoi(argv[3])
+    signal(SIGINT, int_handler);
+    start_server(port);
+    while(1){
+        connection();
+    }
     return 0;
 }
